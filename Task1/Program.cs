d304b14a93a095575e38ddffafd0db2f4ad06cc9
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-07-12 20:08:22 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,4 +4,40 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
 
+double[,] GenerateArray(int rows, int columns)
+{
+    double[,] answer = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            answer[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return answer;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int rows = Prompt("Введите количество строк > ");
+int columns = Prompt("Введите количество столбцов > ");
+double[,] newArray = GenerateArray(rows, columns);
+PrintArray(newArray);
